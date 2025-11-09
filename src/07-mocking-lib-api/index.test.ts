28deeaba6da297be44ebb64bde5eb92d6174dd4c
puzevich <s.rudaya@sam-solutions.com>
2025-11-09 14:01:40 +0300

    feat: mocking-lib-api test implementation

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,54 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    jest.runAllTimers();
+    await throttledGetDataFromApi('/fake_url');
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    jest.runAllTimers();
+    await throttledGetDataFromApi('/provided/url');
+
+    expect(mockGet).toHaveBeenCalledWith('/provided/url');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: 'mock data' });
+    const mockCreate = jest.fn(() => ({ get: mockGet }));
+
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    jest.runAllTimers();
+    const data = await throttledGetDataFromApi('/provided/url');
+
+    expect(data).toEqual('mock data');
   });
 });
